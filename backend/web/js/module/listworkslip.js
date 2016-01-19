dba1731ac9d7c74e42bdcaa7d2b8249664204557
huylv <levanhuy93@gmail.com>
2016-01-19 09:02:31 +0700

    - preview

--- backend/web/js/module/listworkslip.js
+++ backend/web/js/module/listworkslip.js
@@ -14,7 +14,7 @@ var search = function(){
         var request = $.ajax({
             type: 'post',
             data: param,
-            url: baseUrl + '/listworkslip/default/getss',
+            url: baseUrl + '/listworkslip/default/getss'
         });
         var result = request.done(function(data){
             console.log(data);
@@ -34,41 +34,38 @@ var search = function(){
     /* END SCREEN SEARCH */
 
     var validate = function(){
-    //    $('#staff_form').validate({
-    //        groups: {
-    //            name: 'Sdptm08sagyosya[M08_NAME_SEI] Sdptm08sagyosya[M08_NAME_MEI]',
-    //        },
-    //        rules: {
-    //            'Sdptm08sagyosya[M08_JYUG_CD]' : {
-    //                required: true,
-    //                digits: true
-    //            },
-    //            'Sdptm08sagyosya[M08_NAME_SEI]': {
-    //                required: true
-    //            },
-    //            'Sdptm08sagyosya[M08_NAME_MEI]': {
-    //                required: true
-    //            },
-    //            'Sdptm08sagyosya[M08_ORDER]': {
-    //                digits:true
-    //            }
-    //        },
-    //        messages: {
-    //            'Sdptm08sagyosya[M08_JYUG_CD]': {
-    //                required: '従業員CDを入力してください',
-    //                digits: '従業員CDが10桁以内の数字で入力されていない場合'
-    //            },
-    //            'Sdptm08sagyosya[M08_NAME_SEI]': {
-    //                required: '作業者名 姓を入力してください'
-    //            },
-    //            'Sdptm08sagyosya[M08_NAME_MEI]': {
-    //                required: '作業者名 姓を入力してください'
-    //            },
-    //            'Sdptm08sagyosya[M08_ORDER]': {
-    //                digits: '表示順は3文字以内の数字で入力してください'
-    //            }
-    //        }
-    //    });
+        $('#staff_form').validate({
+            groups: {
+                name: 'start_time end_time',
+            },
+            rules: {
+                'start_time' : {
+                    digits: true
+                },
+                'end_time': {
+                },
+                'Sdptm08sagyosya[M08_NAME_MEI]': {
+                    required: true
+                },
+                'Sdptm08sagyosya[M08_ORDER]': {
+                    digits:true
+                }
+            },
+            messages: {
+                'start_time': {
+                    digits: '従業員CDが10桁以内の数字で入力されていない場合'
+                },
+                'end_time': {
+                    required: '作業者名 姓を入力してください'
+                },
+                'Sdptm08sagyosya[M08_NAME_MEI]': {
+                    required: '作業者名 姓を入力してください'
+                },
+                'Sdptm08sagyosya[M08_ORDER]': {
+                    digits: '表示順は3文字以内の数字で入力してください'
+                }
+            }
+        });
     };
 
     var convert_zen2han = function(){
